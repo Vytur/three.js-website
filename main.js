ef72aae415e401d22411375b1a65f2fe88bba72a
Vytur <juknys98@gmail.com>
2024-06-27 12:31:54 +0300

    fixed seed retrieve

--- main.js
+++ main.js
@@ -2,56 +2,64 @@ import * as THREE from "three";
 import { createScene, render } from "./scripts/scene.js";
 import { updateGrid } from "./scripts/grid.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { setupMouseInteraction } from './scripts/interactions.js';
+import { setupMouseInteraction } from "./scripts/interactions.js";
+
+const socket = io("http://localhost:3001");
+
+socket.on("connect", () => {
+  console.log("Connected to server");
+});
 
 const tileSize = 1; // Size of each tile
 const offsetRange = 0.3; // Maximum offset within each tile
-const seed = "1";
 
-// Set up the scene, camera, and renderer
-const { scene, camera, renderer } = createScene();
+socket.on("seed", (seed) => {
 
-setupMouseInteraction(scene, camera);
+  // Set up the scene, camera, and renderer
+  const { scene, camera, renderer } = createScene();
 
-camera.position.set(0, 10, 15);
+  setupMouseInteraction(scene, camera);
 
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = false;
-controls.screenSpacePanning = false;
-controls.enableRotate = false;
-controls.enableZoom = false;
-controls.maxPolarAngle = Math.PI / 2;
+  camera.position.set(0, 10, 15);
 
-const light = new THREE.DirectionalLight(0xffffff, 1);
-light.position.set(0, 2, 2);
-light.castShadow = true;
-light.shadowDarkness = 0.5;
-light.shadowCameraVisible = true;
-scene.add(light);
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = false;
+  controls.screenSpacePanning = false;
+  controls.enableRotate = false;
+  controls.enableZoom = false;
+  controls.maxPolarAngle = Math.PI / 2;
 
-let lastUpdatePosition = new THREE.Vector3(
-  Math.floor(camera.position.x),
-  Math.floor(camera.position.y),
-  Math.floor(camera.position.z)
-);
+  const light = new THREE.DirectionalLight(0xffffff, 1);
+  light.position.set(0, 2, 2);
+  light.castShadow = true;
+  light.shadowDarkness = 0.5;
+  light.shadowCameraVisible = true;
+  scene.add(light);
 
-controls.addEventListener("change", () => {
-  camera.position.y = 10;
-  const currentPosition = new THREE.Vector3(
+  let lastUpdatePosition = new THREE.Vector3(
     Math.floor(camera.position.x),
     Math.floor(camera.position.y),
     Math.floor(camera.position.z)
   );
-  if (
-    currentPosition.x !== lastUpdatePosition.x ||
-    currentPosition.z !== lastUpdatePosition.z
-  ) {
-    updateGrid(scene, camera, tileSize, offsetRange, seed);
-    lastUpdatePosition.copy(currentPosition);
-  }
+
+  controls.addEventListener("change", () => {
+    camera.position.y = 10;
+    const currentPosition = new THREE.Vector3(
+      Math.floor(camera.position.x),
+      Math.floor(camera.position.y),
+      Math.floor(camera.position.z)
+    );
+    if (
+      currentPosition.x !== lastUpdatePosition.x ||
+      currentPosition.z !== lastUpdatePosition.z
+    ) {
+      updateGrid(scene, camera, tileSize, offsetRange, seed);
+      lastUpdatePosition.copy(currentPosition);
+    }
+  });
+
+  updateGrid(scene, camera, tileSize, offsetRange, seed);
+
+  // Render the scene
+  render(renderer, scene, camera);
 });
-
-updateGrid(scene, camera, tileSize, offsetRange, seed);
-
-// Render the scene
-render(renderer, scene, camera);

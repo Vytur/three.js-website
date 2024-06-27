8ea01e1062f9bd6c5d981f0b0549e4126873b46d
Vytur <juknys98@gmail.com>
2024-06-27 11:48:24 +0300

    Added io connection

--- server/server.js
+++ server/server.js
@@ -2,15 +2,28 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIo(server, {
+    cors: {
+        origin: 'http://localhost:5173', 
+        methods: ['GET', 'POST'],
+        allowedHeaders: ['Content-Type'],
+        credentials: true
+    }
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const seed = 1;
+
 io.on('connection', (socket) => {
     console.log('A user connected');
+
+    socket.emit('seed', seed);
 
     socket.on('disconnect', () => {
         console.log('User disconnected');
@@ -25,4 +38,4 @@ io.on('connection', (socket) => {
 const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-});
+}); 
